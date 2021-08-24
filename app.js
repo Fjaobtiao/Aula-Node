f067959e7ee13af260cdd22e2da3b67b8f351c43
Jota Nascimento <jotafn@icloud.com>
2021-08-24 15:24:11 -0300

    Versão 1.0

--- app.js
+++ app.js
@@ -3,13 +3,10 @@
 // git init
 // npm install express
 // npm install -g nodemon
-// npm install ejs
-
-
+// 
 
 // node app.js
 // nodemon app.js
-
 
 const app = require('./config/server')
 
@@ -18,7 +15,7 @@ const noticias = require('./mockup')
 // rota home
 app.get('/', function(req, res){
     // o EJS disponibiliza o metodo render para usar nas respostas das requisições
-    res.render('home/index')
+    res.render('home/index', {noticias: noticias.slice(0, 3)})
 })
 
 // rota notícias
@@ -26,11 +23,18 @@ app.get('/noticias', function(req, res){
     res.render('noticias/noticias', {noticias: noticias})
 })
 
-app.get('/admin' , function(rep, res){
+// rota noticia
+app.get('/noticia', function(req,res){
+    // recupera id noticia por get
+    const id = req.query.id
+    res.render('noticias/noticia', {noticia: noticias[id]})
+})
+
+// rota admin
+app.get('/admin', function(req, res){
     res.render('admin/form_add_noticias')
 })
 
 app.listen(3000, () => {
     console.log('Servidor rodando com express')
 })
-
