ac211d969d5d7604e8ac590e12806ded6e836918
Jota Nascimento <jotafn@icloud.com>
2021-08-24 22:49:32 -0300

    configurações heroko deploy

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@
 const { Client } = require('pg')
 
 const client = new Client({
-    connectionString: 'postgres://repaerloijrhpx:9e6dc8af488e48a20522b40832679d5e4f35f4168e6e94b0d5e31f08d4748f73@ec2-35-174-122-153.compute-1.amazonaws.com:5432/d1e685bt2rn37',
+    connectionString: process.env.DATABASE_URL || 'postgres://repaerloijrhpx:9e6dc8af488e48a20522b40832679d5e4f35f4168e6e94b0d5e31f08d4748f73@ec2-35-174-122-153.compute-1.amazonaws.com:5432/d1e685bt2rn37',
     ssl: {
         rejectUnauthorized: false
     }
